cafabfc067208879c3156be581dbc483f086433a
Rodrigo Esteban Beltran Abello - Ceiba Software <esteban.beltran@ceiba.com.co>
2022-05-17 10:04:02 -0500

    Se agrego el primer servicio guiado al dominio

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -1,39 +1,56 @@
 package com.platzi.market.persistence;
 
+import com.platzi.market.domain.Product;
 import com.platzi.market.domain.repository.ProductRepository;
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import com.platzi.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository{
+public class ProductoRepository implements ProductRepository{
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
 
-    public List<Producto> getAll(){
-        return (List<Producto>) this.productoCrudRepository.findAll();
+    @Autowired
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) this.productoCrudRepository.findAll();
+        return this.mapper.toProducts(productos);
     }
 
-    public List<Producto> getByCategoria(Integer idCategoria){
-        return this.productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(Integer categoryId) {
+        List<Producto> productos = this.productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(this.mapper.toProducts(productos));
     }
 
-    public Optional<List<Producto>> getEscasos(Integer cantidadStock){
-        return this.productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
+    @Override
+    public Optional<List<Product>> getScarseProducts(Integer quantity) {
+        Optional<List<Producto>> productos = this.productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> this.mapper.toProducts(prods));
     }
 
-    public Optional<Producto> getProducto(Integer idProducto){
-        return this.productoCrudRepository.findById(idProducto);
+    @Override
+    public Optional<Product> getProduct(Integer productId) {
+        return this.productoCrudRepository.findById(productId).map(producto -> this.mapper.toProduct(producto));
     }
 
-    public Producto save(Producto producto){
-        return this.productoCrudRepository.save(producto);
+    @Override
+    public Product save(Product product) {
+        Producto producto = this.mapper.toProducto(product);
+        return this.mapper.toProduct(this.productoCrudRepository.save(producto));
     }
 
-    public void delete(Integer idProducto){
-        this.productoCrudRepository.deleteById(idProducto);
+    @Override
+    public void delete(Integer productId){
+        this.productoCrudRepository.deleteById(productId);
     }
 }
